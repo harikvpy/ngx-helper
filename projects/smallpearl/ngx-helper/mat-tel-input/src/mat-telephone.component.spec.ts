c5e1dda7f4e8387c18421daf1bd9f8aea3440072
Hari Mahadevan <hari@sutlej.local>
2024-09-04 11:11:16 +0800

    fdescribe -> describe

--- projects/smallpearl/ngx-helper/mat-tel-input/src/mat-telephone.component.spec.ts
+++ projects/smallpearl/ngx-helper/mat-tel-input/src/mat-telephone.component.spec.ts
@@ -4,7 +4,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { SPMatTelephoneInputComponent } from "./mat-telephone.component";
 
-fdescribe('MatTelephoneInputComponent', () => {
+describe('MatTelephoneInputComponent', () => {
   let component: SPMatTelephoneInputComponent;
   let fixture: ComponentFixture<SPMatTelephoneInputComponent>;
 
