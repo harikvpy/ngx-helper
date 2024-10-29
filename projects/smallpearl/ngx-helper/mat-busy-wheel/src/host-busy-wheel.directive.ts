fe2ee5cd147837c2bdfd2139eb6edd9f8e8e052d
Hari Mahadevan <hari@sutlej.local>
2024-10-29 16:46:40 +0800

    Add comment showing how to use the spHostBusyWheel directive

--- projects/smallpearl/ngx-helper/mat-busy-wheel/src/host-busy-wheel.directive.ts
+++ projects/smallpearl/ngx-helper/mat-busy-wheel/src/host-busy-wheel.directive.ts
@@ -2,7 +2,27 @@ import { Directive, input, Input, OnDestroy, OnInit, Renderer2, ViewContainerRef
 import { deregisterBusyWheelHost, registerBusyWheelHost } from './busy-wheel.service';
 
 /**
+ * Use this directive with a unique value assigned to it to display a busy
+ * wheel over it. This can be used as:-
  *
+ * import {showBusyWheelUntilComplete} from '@smallpearl/ngx-helper/mat-busy-wheel';
+ *
+ * <div spHostBusyWheel='myDivBusyWheel'>
+ *  ..content
+ * </div>
+ *
+ * Then in your code:
+ *
+ * obs.pipe(
+ *  showBusyWheelUntilComplete('myDivBusyWheel'),
+ *  tap((resp) => {
+ *    ...
+ *  })
+ * ).subscribe();
+ *
+ * This will cover the 'div' above when the obs is waiting to be completed.
+ * (or error). Note that you can also use the trackBusyWheelStatus() function
+ * if you want more control over the busy wheel.
  */
 @Directive({
   selector: '[spHostBusyWheel]',
