918db47303df052e63b4012eee3ff27cff47db1e
Hari Mahadevan <hari@sutlej.local>
2025-02-18 10:07:18 +0800

    Remove redundant legacy modules

--- projects/smallpearl/ngx-helper/mat-busy-wheel/index.ts
+++ projects/smallpearl/ngx-helper/mat-busy-wheel/index.ts
@@ -1,5 +1,4 @@
 export * from './src/busy-wheel-op';
 export * from './src/busy-wheel.component';
 export * from './src/busy-wheel.service';
-export * from './src/host-busy-wheel.directive'
-export * from './src/busy-wheel.module';
+export * from './src/host-busy-wheel.directive';
--- projects/smallpearl/ngx-helper/mat-form-error/index.ts
+++ projects/smallpearl/ngx-helper/mat-form-error/index.ts
@@ -3,5 +3,4 @@ export * from './src/ngx-mat-error-def.directive';
 export * from './src/ngx-mat-errors-for-date-range-picker.directive';
 export * from './src/ngx-mat-errors.component';
 export * from './src/ngx-error-list.component';
-// export * from './src/ngx-mat-errors.module';
 export type * from './src/types';
